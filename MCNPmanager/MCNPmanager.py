557421f9deed975ba8da716117e81fe25c7f5b44
ipostuma <ianpostuma@icloud.com>
2016-06-07 18:08:25 +0200

    updated bug path name

--- MCNPmanager/MCNPmanager.py
+++ MCNPmanager/MCNPmanager.py
@@ -16,10 +16,8 @@ def initCreateDir(directory):
                 path.join(this_dir,"cards/tallies.part"),
                 path.join(this_dir,"cards/traslations.part")]
     for card in cards:
-        print "move "+card+" in "+path.join(directory, "cards/",path.basename(directory))
-        #copyfile(card, path.join(directory, "cards/",path.basename(directory)))
+        copyfile(card, path.join(directory, "cards/",path.basename(card)))
     geom=[  path.join(this_dir,"cards/cells.part"),
             path.join(this_dir,"cards/surfaces.part")]
     for g in geom:
-        print "move "+g+" in "+path.join(directory, "geom/",path.basename(g))
-        #copyfile(g, path.join(directory, "geom/",path.basename(g)))
+        copyfile(g, path.join(directory, "geom/",path.basename(g)))
