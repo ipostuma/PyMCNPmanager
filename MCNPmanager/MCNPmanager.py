d65c384fe9653da7d5ea2ec6cfde703d4a9d199e
ipostuma <ianpostuma@icloud.com>
2016-06-07 17:58:54 +0200

    module file directory fetched with path.split(__file__)

--- MCNPmanager/MCNPmanager.py
+++ MCNPmanager/MCNPmanager.py
@@ -3,20 +3,21 @@ from os import path, makedirs
 from shutil import copyfile
 
 def initCreateDir(directory):
+    this_dir, this_filename = path.split(__file__)
     if not path.exists(directory):
         makedirs(directory)
     if not path.exists(path.join(directory, "cards")):
         makedirs(path.join(directory, "cards"))
     if not path.exists(path.join(directory, "geom")):
         makedirs(path.join(directory, "geom"))
-    cards = [   "cards/parameters.part",
-                "cards/materials.part",
-                "cards/source.part",
-                "cards/tallies.part",
-                "cards/traslations.part"]
+    cards = [   path.join(this_dir,"cards/parameters.part"),
+                path.join(this_dir,"cards/materials.part"),
+                path.join(this_dir,"cards/source.part"),
+                path.join(this_dir,"cards/tallies.part"),
+                path.join(this_dir,"cards/traslations.part")]
     for card in cards:
         copyfile(card, path.join(directory, "cards"))
-    geom=[  "geom/cells.part",
-            "geom/surfaces.part"]
+    geom=[  path.join(this_dir,"geom/cells.part"),
+            path.join(this_dir,"geom/surfaces.part")]
     for g in geom:
         copyfile(g, path.join(directory, "geom"))
