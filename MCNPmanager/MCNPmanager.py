ce95dd7b6826b7e570dd42f1d3fc248a3656c788
ipostuma <ianpostuma@icloud.com>
2016-06-08 15:30:14 +0200

    cpMCNPproject error flow update

--- MCNPmanager/MCNPmanager.py
+++ MCNPmanager/MCNPmanager.py
@@ -2,6 +2,16 @@ import sys, string
 from os import path, makedirs,getcwd,remove
 from shutil import copyfile
 
+#This function manages the creation of the MCNP project by creating 2 directories
+#   - cards : which contains files for the definition of
+#           * parameters
+#           * materials
+#           * source
+#           * tallies
+#           * translations
+#   - geom  : which contains files for the definition of
+#           * cells
+#           * surfaces
 def initCreateDir(directory):
     this_dir, this_filename = path.split(__file__)
     if not path.exists(directory):
@@ -17,24 +27,17 @@ def initCreateDir(directory):
                 path.join(this_dir,"cards/traslations.part")]
     for card in cards:
         copyfile(card, path.join(directory, "cards/",path.basename(card)))
-    geom=[  path.join(this_dir,"cards/cells.part"),
-            path.join(this_dir,"cards/surfaces.part")]
+    geom  = [   path.join(this_dir,"cards/cells.part"),
+                path.join(this_dir,"cards/surfaces.part")]
     for g in geom:
         copyfile(g, path.join(directory, "geom/",path.basename(g)))
 
+#This function defines a method to copy an existing project into an other project (a sort of new branch)
 def cpMCNPproject(directory):
     wkdir=getcwd()
-    if not path.exists(path.join(directory, "cards")):
-        print "\n\033[1;34mMCNPmanager cp error:\033[1;32m %s contains no MCNPmanager project\033[0m\n" % (directory)
+    if checkifMCNPproject(directory,1)==1:
         return 1
-    elif not path.exists(path.join(directory, "geom")):
-        print "\n\033[1;34mMCNPmanager cp error:\033[1;32m %s contains no MCNPmanager project\033[0m\n" % (directory)
-        return 1
-    elif not path.exists(path.join(wkdir, "cards")):
-        print "\n\033[1;34mMCNPmanager cp error:\033[1;32m you are in %s ad there is no MCNPmanager project\033[0m\n" % (wkdir)
-        return 2
-    elif not path.exists(path.join(wkdir, "geom")):
-        print "\n\033[1;34mMCNPmanager cp error:\033[1;32m you are in %s ad there is no MCNPmanager project\033[0m\n" % (wkdir)
+    if checkifMCNPproject(wkdir,2)==2:
         return 2
     else:
         cards = [   path.join(directory,"cards/parameters.part"),
@@ -56,3 +59,11 @@ def cpMCNPproject(directory):
             except Exception as e:
                 print "\n\033[1;34mMCNPmanager cp error:\033[1;32m %s \033[0m\n" % (e)
         return 0
+
+def checkifMCNPproject(directory,r):
+    if not path.exists(path.join(directory, "cards")):
+        print "\n\033[1;34mMCNPmanager cp error:\033[1;32m %s contains no MCNPmanager project\033[0m\n" % (directory)
+        return r
+    elif not path.exists(path.join(directory, "geom")):
+        print "\n\033[1;34mMCNPmanager cp error:\033[1;32m %s contains no MCNPmanager project\033[0m\n" % (directory)
+        return r
